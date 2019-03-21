f027f45378e896d6f86ed0cbad1547a872fadaf3
Allyn <44602686+allynkalda@users.noreply.github.com>
2019-03-21 21:40:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ var random = Math.floor((Math.random() * 20) + 1);
 var numGuess = 0;
 var allNumbers = [];
 
+document.getElementById("button-2").style.display = "none";
+
+console.log(random);
 function startGame() {
 
 
 var userInput = document.getElementById("number").value; // Get user's guess
 
-allNumbers.push(userInput);
-// Check if there is no number or number is too big
+
+allNumbers.unshift(userInput);
+// Check if there is no number or too big
 
 if (userInput == "") {
 	alert("Type a number!");
@@ -20,37 +24,41 @@ if (userInput == "") {
 }
 
 // Comparison
-	
 function bigOrSmall() {
 
-	if (userInput == random) {
-		document.getElementById("demo").innerHTML = "YOU WON! Congratulations.";
-		location.reload();
-	} else if (userInput < random) {
-		document.getElementById("demo").innerHTML = "This is smaller than the secret number! Try again.";
-		//document.getElementById("number").value = "";
-		
-		checkSameNum()
-
-		guessInfo();
-		document.getElementById("guesses").innerHTML = "Guess Count: " + numGuess;
-	} else if (userInput > random) {
-		document.getElementById("demo").innerHTML = "This is bigger than the secret number! Try again.";
-		//document.getElementById("number").value = "";
-		checkSameNum()
-
-
-		guessInfo();
-		document.getElementById("guesses").innerHTML = "Guess Count: " + numGuess;
-
 // Check if the number is the same inputted multiple times consecutively.
-
 function checkSameNum() {
+
 	if (userInput == allNumbers[1]) {
 			numGuess += 0;
 	} else {
-			numGuess += 1; } 
+			numGuess += 1; }
 	}
+
+
+	if (userInput == random) {
+		document.getElementById("demo").innerHTML = "YOU WON! Congratulations.";
+		document.getElementById("button-1").style.display = "none";
+		document.getElementById("button-2").style.display = "inline";
+	//	document.getElementById("button").onclick = location.reload();
+		
+	} else if (userInput < random) {
+		checkSameNum();
+		document.getElementById("demo").innerHTML = "This is smaller than the secret number! Try again.";
+		//document.getElementById("number").value = "";
+		
+	
+		guessInfo();
+		document.getElementById("guesses").innerHTML = "Guess Count: " + numGuess;
+	} else if (userInput > random) {
+		checkSameNum();
+		document.getElementById("demo").innerHTML = "This is bigger than the secret number! Try again.";
+		//document.getElementById("number").value = "";
+
+		guessInfo();
+		document.getElementById("guesses").innerHTML = "Guess Count: " + numGuess;
+
+
 
 	}
 
