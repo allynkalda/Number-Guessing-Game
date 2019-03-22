93b49c2ceef9a1b187f07bfb9f2ee26391b73c58
Allyn <44602686+allynkalda@users.noreply.github.com>
2019-03-22 09:14:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var userInput = document.getElementById("number").value; // Get user's guess
 allNumbers.unshift(userInput);
 // Check if there is no number or too big or not a number
 
-if (userInput == "" || typeof userInput !== "number") {
+if (userInput == "") {
 	alert("Type a number!");
 } else if (userInput > 20) {
 	alert("Number is too big!");
